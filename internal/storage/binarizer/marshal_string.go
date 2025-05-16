c648b5b75d2a5029faf8b4aa56ff5aa71d779a1a
p4xt3r <121817930+algrvvv@users.noreply.github.com>
2025-05-16 15:15:12 +0300

    реализован маршал стринга

---
+++ internal/storage/binarizer/marshal_string.go
@@ -0,0 +1,31 @@
+package binarizer
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+)
+
+func MarshalString(w *bytes.Buffer, value string) error {
+	const op = "binarizer.MarshalString"
+
+	err := w.WriteByte(StrType)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	// преобразуем в uint16
+	// так как минимальный размер + не отрицательное
+	strlen := uint16(len(value))
+	err = binary.Write(w, binary.LittleEndian, strlen)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	_, err = w.Write([]byte(value))
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
+}

0855ff72cde2d5bbe0e2df418621898d4ae91175
p4xt3r <121817930+algrvvv@users.noreply.github.com>
2025-05-19 22:49:21 +0300

    добавлена шаблон ошибки при попытке конвертировать неверный тип данных

--- internal/storage/binarizer/errors.go
+++ internal/storage/binarizer/errors.go
@@ -9,3 +9,13 @@ type ErrInvalidDataType struct {
 func (e *ErrInvalidDataType) Error() string {
 	return fmt.Sprintf("invalid data type for %q key", e.Key)
 }
+
+type ErrTypeMismatch struct {
+	Key  string
+	Got  any
+	Want string
+}
+
+func (e *ErrTypeMismatch) Error() string {
+	return fmt.Sprintf("type mismatch for %q: got: %T; want: %v", e.Key, e.Got, e.Want)
+}
